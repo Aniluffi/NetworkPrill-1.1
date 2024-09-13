77d5bb9f0ccb0e2098ef78d4a6e547e4c1d1b396
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2024-09-13 15:42:03 +0300

    Add files via upload

---
+++ Client2/Client2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+
+class Program
+{
+    public static async Task Main()
+    {
+        using var tcpClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        try
+        {
+            await tcpClient.ConnectAsync("127.0.0.1", 808);
+
+
+            while (true)
+            {
+                Console.WriteLine("Send:");
+                string word = Console.ReadLine();
+                byte[] data = Encoding.UTF8.GetBytes(word);
+
+                await tcpClient.SendAsync(data);
+
+                var buf = new byte[1024];
+                int acceptBytes = await tcpClient.ReceiveAsync(buf);
+
+                string acceptWord = Encoding.UTF8.GetString(buf, 0, acceptBytes);
+                Console.WriteLine($"{acceptWord}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+    }
+}
---
+++ DOCS2/DOCS2/Server.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DOCS2
+{
+    class Server
+    {
+        List<Socket> clients = new List<Socket>();
+
+
+        public async Task ServerOn()
+        {
+            using var tcpLisener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                tcpLisener.Bind(new IPEndPoint(IPAddress.Any, 808));
+                tcpLisener.Listen();    // запускаем сервер
+                Console.WriteLine("Сервер запущен. Ожидание подключений... ");
+
+                while (true)
+                {
+                    var tcpClient = await tcpLisener.AcceptAsync();
+                    lock (clients)
+                    {
+                        clients.Add(tcpClient); // Добавление клиента в список
+                    }
+                    Console.WriteLine($"{tcpClient.RemoteEndPoint} подключился");
+
+
+                    await Task.Run(async () => Client(tcpClient));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+
+        private async Task Client(Socket tcpClient)
+        {
+            var buffer = new byte[1024];
+
+            try
+            {
+                while (true)
+                {
+                    var bytesRead = await tcpClient.ReceiveAsync(buffer);
+
+                    var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    Console.WriteLine($"Сообщение от {tcpClient.RemoteEndPoint}: {message}");
+
+                    await BroadCast(tcpClient, message);
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine($"Клиент {tcpClient.RemoteEndPoint} отключился");
+            }
+
+        }
+
+        private async Task BroadCast(Socket tcpClient, string message)
+        {
+            try
+            {
+                foreach (var client in clients)
+                {
+                    var mess = Encoding.UTF8.GetBytes($"\n Сообщение от {tcpClient.RemoteEndPoint}:  " + message);
+                    if (tcpClient.RemoteEndPoint != client.RemoteEndPoint)
+                        await client.SendAsync(mess);
+                }
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Ошибка рассылки");
+            }
+
+        }
+        }
+    }
